ad2abcd5e9f15c2560bd5305c3a5b04168818304
tiendo123 <tiendo987@gmail.com>
2021-06-24 00:38:05 +0700

    done trainee

--- ApplicationDevelopment/Controllers/TraineeController.cs
+++ ApplicationDevelopment/Controllers/TraineeController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
@@ -52,6 +53,87 @@ namespace ApplicationDevelopment.Controllers
             {
                 _userManager = value;
             }
+        }
+        public ActionResult Edit()
+        {
+            string userIdValue = String.Empty;
+            var claimsIdentity = User.Identity as ClaimsIdentity;
+            if (claimsIdentity != null)
+            {
+                var userIdClaim = claimsIdentity.Claims
+                    .FirstOrDefault(x => x.Type == ClaimTypes.NameIdentifier);
+
+                if (userIdClaim != null)
+                {
+                    userIdValue = userIdClaim.Value;
+                }
+            }
+            var user = _db.Trainees.Find(userIdValue);
+            return View(user);
+        }
+        [HttpPost]
+        public ActionResult Edit(Trainee trainee)
+        {
+            var tranerUser = _db.Users.Find(trainee.Id);
+
+            if (tranerUser == null)
+            {
+                ViewData["Message"] = "Error: User not already exists";
+                return View(trainee);
+            }
+            var traineeProfile = _db.Trainees.Find(trainee.Id);
+            traineeProfile.FullName = trainee.FullName;
+            traineeProfile.DateOfBirth = trainee.DateOfBirth;
+            traineeProfile.Education = trainee.Education;
+            traineeProfile.MainProgrammingLanguage = trainee.MainProgrammingLanguage;
+            traineeProfile.ToeicScore = trainee.ToeicScore;
+            traineeProfile.ExperimentDetail = trainee.ExperimentDetail;
+            traineeProfile.Location = trainee.Location;
+            traineeProfile.Department = trainee.Department;
+            traineeProfile.PhoneNumber = trainee.PhoneNumber;
+
+            _db.Entry(traineeProfile).State = EntityState.Modified;
+            _db.SaveChanges();
+            ViewData["Message"] = "Success update profile";
+            return View(trainee);
+        }
+
+        public ActionResult CourseEnrolled()
+        {
+            string userIdValue = String.Empty;
+            var claimsIdentity = User.Identity as ClaimsIdentity;
+            if (claimsIdentity != null)
+            {
+                var userIdClaim = claimsIdentity.Claims
+                    .FirstOrDefault(x => x.Type == ClaimTypes.NameIdentifier);
+
+                if (userIdClaim != null)
+                {
+                    userIdValue = userIdClaim.Value;
+                }
+            }
+            return View(_db.Enrolls.Where(c => c.TraineeId == userIdValue).Include(c => c.Course).ToList());
+        }
+
+        public ActionResult CourseAvailable()
+        {
+            string userIdValue = String.Empty;
+            var claimsIdentity = User.Identity as ClaimsIdentity;
+            if (claimsIdentity != null)
+            {
+                var userIdClaim = claimsIdentity.Claims
+                    .FirstOrDefault(x => x.Type == ClaimTypes.NameIdentifier);
+
+                if (userIdClaim != null)
+                {
+                    userIdValue = userIdClaim.Value;
+                }
+            }
+            List<Course> availablecourse = new List<Course>();
+            var allcourse = _db.Courses.ToList();
+            var enrollments = _db.Enrolls.Where(c => c.TraineeId == userIdValue).Include(c => c.Course).ToList();
+            availablecourse = allcourse.Except(allcourse.Where(i => enrollments.Select(o => o.CourseId).ToList().Contains(i.Id))).ToList();
+            return View(availablecourse);
         }
     }
 }
