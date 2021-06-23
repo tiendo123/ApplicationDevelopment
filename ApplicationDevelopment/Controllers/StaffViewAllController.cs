7a300628d80acba02e4cb59a1d3694dee56c7438
tiendo123 <tiendo987@gmail.com>
2021-06-23 23:04:10 +0700

    view all students TEACHER IN CLASS

---
+++ ApplicationDevelopment/Controllers/StaffViewAllController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using ApplicationDevelopment.Models;
+using ApplicationDevelopment.ViewModels;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNet.Identity.Owin;
+
+
+namespace ApplicationDevelopment.Controllers
+{
+    [Authorize(Roles = "Staff")]
+    public class StaffViewAllController : Controller
+    {
+        private ApplicationDbContext _db;
+        private void AddErrors(IdentityResult result)
+        {
+            foreach (var error in result.Errors)
+            {
+                ModelState.AddModelError("", error);
+            }
+        }
+        public StaffViewAllController()
+        {
+            _db = new ApplicationDbContext();
+        }
+        public ActionResult SelectCourse()
+        {
+            return View(_db.Courses.Include(t => t.Category).ToList());
+        }
+        public ActionResult StaffViewAll(int Id)
+        {
+            StaffViewAllVm staffViewAllVm = new StaffViewAllVm();
+            staffViewAllVm.TraineeList = _db.Enrolls.Where(e => e.CourseId == Id).Include(e => e.Trainee);
+            staffViewAllVm.TrainerList = _db.Assigns.Where(a => a.CourseId == Id).Include(a => a.Trainer);
+
+            return View(staffViewAllVm);
+        }
+    }
+}
---
+++ ApplicationDevelopment/ViewModels/StaffViewAllVm.cs
@@ -0,0 +1,14 @@
+﻿using ApplicationDevelopment.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ApplicationDevelopment.ViewModels
+{
+    public class StaffViewAllVm
+    {
+        public IEnumerable<Assign> TrainerList { get; set; }
+        public IEnumerable<Enroll> TraineeList { get; set; }
+    }
+}
